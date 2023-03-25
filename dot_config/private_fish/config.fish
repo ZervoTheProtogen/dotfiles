f330377f91a7cc3580518ac941c2086665eaaa05
Vermium Sifell <vermium@zyner.org>
2023-03-25 13:08:29 +0100

    update

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,3 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+bind \e\[3\;5~ kill-word
+bind \cH backward-kill-word
+bind \cV beginning-of-line
+bind \f end-of-line
